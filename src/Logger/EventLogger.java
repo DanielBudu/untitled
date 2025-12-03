479f3990752fc1cbd10de5348c4f03991f4adae2
Madra_Ropucha <szymon291203@gmail.com>
2025-12-03 23:31:26 +0100

    feat: added a List<String> as logger

--- src/Logger/EventLogger.java
+++ src/Logger/EventLogger.java
@@ -3,12 +3,17 @@ package Logger;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventLogger {
 
     private static final String FILE_NAME = "thermostat.log";
+    private static final List<String> log = new ArrayList<>();
 
     public static void write(String msg) {
+        log.add(msg);
+
         try (FileWriter fw = new FileWriter(FILE_NAME, true)) {
             fw.write(LocalDateTime.now() + " - " + msg + System.lineSeparator());
         } catch (IOException e) {
