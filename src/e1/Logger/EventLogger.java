a4cd78e932c22d217b4bdbe0fc3c4f85f939d48a
Madra_Ropucha <szymon291203@gmail.com>
2025-12-04 16:31:13 +0100

    feat: given format to time on log

--- src/e1/Logger/EventLogger.java
+++ src/e1/Logger/EventLogger.java
@@ -3,6 +3,7 @@ package e1.Logger;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class EventLogger {
         log.add(msg);
 
         try (FileWriter fw = new FileWriter(FILE_NAME, true)) {
-            fw.write(LocalDateTime.now() + " - " + msg + System.lineSeparator());
+            fw.write(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + " - " + msg + System.lineSeparator());
         } catch (IOException e) {
             System.out.println("Error guardando log: " + e.getMessage());
         }
